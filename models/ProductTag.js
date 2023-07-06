cfff12a1bf730df873461d484d92584fa4daf33a
geoffwithers <geoffwithers91@gmail.com>
2023-07-06 18:48:29 -0400

    completed models pages

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,27 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    // this page defines columbs for the product_tag table
+    id: {
+      type: DataTypes.INTEGER, // Sets the data type to INTEGER
+      allowNull: false, // Disallows NULL values
+      primaryKey: true, // Set as the primary key
+      autoIncrement: true, // Automatically increments values
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, // Sets the data type to INTEGER
+      references: {
+        model: "tag", // References the 'tag' table
+        key: "id", // References the 'id' column found in the 'tag' table
+      },
+    },
+    product_id: {
+      type: DataTypes.INTEGER, // Sets the data type to INTEGER
+      references: {
+        model: "product", // References the 'product' table
+        key: "id", // References the 'id' column found in the 'product' table
+      },
+    },
   },
   {
     sequelize,
